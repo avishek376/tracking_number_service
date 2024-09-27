d309892aa19653ba5455f9997a7df1cae90f9266
avishek376 <avishekdeb376@gmail.com>
2024-09-27 08:11:19 +0530

    initial commit

--- tracking/utils.py
+++ tracking/utils.py
@@ -2,6 +2,6 @@ import random
 import string
 
 
-def generate_tracking_number():
+async def generate_tracking_number():
     characters = string.ascii_uppercase + string.digits
     return ''.join(random.choices(characters, k=16))
